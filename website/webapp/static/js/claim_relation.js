1e6101146dbf4e54cd7c6c4a7e9ef37589318f41
schen149 <schen149@illinois.edu>
2018-10-02 16:59:31 -0400

    added submit function (not going into db)

--- website/webapp/static/js/claim_relation.js
+++ website/webapp/static/js/claim_relation.js
@@ -4,7 +4,33 @@ $(document).ready(function () {
         $("#content").toggle();
     });
 
-    $('#neg_option_clear').click(function() {
+    $('#rel_option_clear').click(function() {
         $('.rel_option').prop('checked', false);
     });
+    $('#rel_option_submit').click(submit);
 });
+
+/**
+ * Submit the annotations to backend
+ */
+function submit() {
+
+    // Get all annotations
+    let el_persps = $('.rel_radio_container');
+    let annos = [];
+    el_persps.each(function() {
+        $(this).find('input:checked').each(function() {
+            let el_id = $(this).attr('id').split("_");
+            let persp_id = el_id.pop();
+            let rel = el_id.pop();
+            annos.push([persp_id, rel]);
+        });
+    });
+
+    if (el_persps.length !== annos.length) {
+        alert("Please annotate all questions!");
+        return;
+    }
+
+    console.log('Success!')
+}
