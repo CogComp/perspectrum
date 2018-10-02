9a1a8caaf78f23b590970b6eed04be74ff963074
schen149 <schen149@illinois.edu>
2018-10-02 10:48:46 -0400

    Added perspective pool selection function

--- website/webapp/views.py
+++ website/webapp/views.py
@@ -3,23 +3,38 @@ import json
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import render
+from .models import *
 
 from .util.helper import get_all_claim_title_id, get_claim_given_id
 file_names = {
     "iDebate": '../data/idebate/idebate.json'
 }
 
-
+"""Helper functions"""
 def load_json(file_name):
     with open(file_name, encoding='utf-8') as data_file:
         data = json.loads(data_file.read())
         return data
 
-
 def get_json(request):
     data = load_json(file_names["iDebate"])
     return JsonResponse({"data": data})
 
+
+def get_pool_from_claim_id(claim_id):
+    """
+    TODO: Change this function! Right now it's only for testing purpose
+    :param claim_id: id of the claim
+    :return:
+    """
+    related_persp_anno = RelationAnnotation.objects.filter(author=RelationAnnotation.GOLD, claim_id=claim_id).\
+        order_by("?")[:2]
+    related_persps = [Perspective.objects.get(id=rel.perspective_id) for rel in related_persp_anno]
+
+    return related_persps
+
+
+""" APIs """
 def main_page(request):
     context = {
         "datasets": list(file_names.keys())
@@ -50,6 +65,16 @@ def vis_persps(request, claim_id):
 def vis_neg_anno(request, claim_id):
     return render(request, 'claim_neg_anno.html', {})
 
+
 def vis_relation(request, claim_id):
-    return render(request, 'claim_relation.html', {})
+    try:
+        claim = Claim.objects.get(id=claim_id)
+    except Claim.DoesNotExist:
+        pass  # TODO: Do something?
+
+    perspective_pool = get_pool_from_claim_id(claim_id)
+
+    return render(request, 'claim_relation.html', {
+        "perspective_pool": perspective_pool
+    })
 
