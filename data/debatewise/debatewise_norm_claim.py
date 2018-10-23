bb7a8be28e2f4e4abaea08ec6c6f96f94cf82469
schen149 <schen149@illinois.edu>
2018-10-23 15:39:59 -0400

    add filtered debatewise data

--- data/debatewise/debatewise_norm_claim.py
+++ data/debatewise/debatewise_norm_claim.py
@@ -12,17 +12,28 @@ input_json = sys.argv[2]
 out_json = sys.argv[3]
 
 
-with open(norm_claim, 'r') as fin, open(input_json, 'r') as json_in, open(out_json, 'w') as fout:
+with open(norm_claim, 'r', encoding='utf-8') as fin, open(input_json, 'r', encoding='utf-8') as json_in, open(out_json, 'w', encoding='utf-8') as fout:
     norm_mapping = {}
     reader = csv.DictReader(fin)
     for row in reader:
         norm_mapping[row["orig"].strip()] = row["normalized"].strip()
 
     data = json.load(json_in)
+    print(len(data))
 
-    for c in data:
+    drop_list = []
+    for idx, c in enumerate(data):
         if c["claim_title"] in norm_mapping:
-            c["claim_title"] = norm_mapping[c["claim_title"]]
+            normed_claim = norm_mapping[c["claim_title"]]
+            if normed_claim:
+                c["claim_title"] = normed_claim
+            else:
+                drop_list.append(idx)
+
+    for i in sorted(drop_list, reverse=True):
+        del data[i]
+
+    print(len(data))
 
     json.dump(data, fout)
 
