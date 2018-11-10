f5f368f6f0c1280fd438282cebf42f0759d023a6
schen149 <schen149@illinois.edu>
2018-11-09 20:22:52 -0500

    Added notebooks; edited google search script

--- persp_crawler/websearch/google_search.py
+++ persp_crawler/websearch/google_search.py
@@ -2,6 +2,8 @@
 Google custom search API
 """
 import requests
+import json
+import time
 
 API_URL = "https://www.googleapis.com/customsearch/v1"
 
@@ -32,12 +34,34 @@ class GoogleSearch:
 if __name__ == '__main__':
     import sys
 
-    if len(sys.argv) != 3:
-        print("Usage: python ... [api_key] [cx]", file=sys.stderr)
+    if len(sys.argv) != 5:
+        print("Usage: python ... [api_key] [cx] [claim_persp_json] [out_path]", file=sys.stderr)
         exit(1)
 
     api_key = sys.argv[1]
     cx = sys.argv[2]
+    claim_persp_path = sys.argv[3]
+    out_path = sys.argv[4]
+
     gs = GoogleSearch(api_key, cx)
-    res = gs.search('We Should Have a Quota for Women on Corporate Boards. Putting a Quota in Place Would Help Women')
-    print(res)
+
+    # Get all valid repsonses from before
+    with open(out_path, 'r', encoding='utf-8') as fin:
+        resps = json.load(fin)
+        resps = [p for p in resps if "error" not in p]
+        processed_pids = {res['perspective_id'] for res in resps}
+
+    with open(claim_persp_path, 'r', encoding='utf-8') as fin:
+        cps = json.load(fin)
+
+    for cp in cps:
+        id = cp['id']
+        if id not in processed_pids:
+            print('Searching perspective id = {}'.format(id))
+            res = gs.search(cp['title'], start_index=2)
+            res['perspective_id'] = id
+            resps.append(res)
+            time.sleep(0.8)
+
+    with open(out_path, 'w') as fout:
+        json.dump(resps, fout)
