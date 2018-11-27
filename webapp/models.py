5979f72570ea99b79e5f7a29226eb0a63c0af37b
schen149 <schen149@illinois.edu>
2018-11-27 15:13:36 -0500

    Added labels fields in re-step1 results

--- webapp/models.py
+++ webapp/models.py
@@ -97,6 +97,20 @@ class EquivalenceAnnotation(models.Model):
 
 # Experiment results
 class ReStep1Results(models.Model):
+    LABEL_5_CHOICES = (
+        ('S', 'Support'),
+        ('U', 'Undermine'),
+        ('A', 'Slight Support'),
+        ('B', 'Slight Undermine'),
+        ('N', 'Not Valid')
+    )
+
+    LABEL_3_CHOICES = (
+        ('S', 'Support'),
+        ('U', 'Undermine'),
+        ('N', 'Not Valid')
+    )
+
     claim_id = models.IntegerField()
     perspective_id = models.IntegerField()
     vote_support = models.IntegerField(default=0)
@@ -104,6 +118,8 @@ class ReStep1Results(models.Model):
     vote_leaning_undermine = models.IntegerField(default=0)
     vote_undermine = models.IntegerField(default=0)
     vote_not_valid = models.IntegerField(default=0)
+    label_5 = models.CharField(default='N', max_length=1, choices=LABEL_5_CHOICES)
+    label_3 = models.CharField(default='N', max_length=1, choices=LABEL_3_CHOICES)
     p_i_5 = models.FloatField(default=0)
     p_i_3 = models.FloatField(default=0)
 
